3ae1ceeca76f2dfdf42a72a8cfb80b0655a2e034
YoeungYeng <yoeungyeng760@gmail.com>
2024-06-20 17:48:40 +0700

    first commit

---
+++ Home/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn PHP</title>
+</head>
+<body>
+    <?php 
+        
+        // $a = 55; 
+        // $b = 55;
+        // $c = $a + $b;
+        // echo "The Sum:", $c;
+        // // while loop
+        // $i = 1;
+        // while($i < 10){
+            
+        //     echo "Sokrong, I Love You. <br/>"; 
+        //     $i++;
+        // }
+        echo "My is the Colors<br/>";
+        $colors = array("red", "blue", "green", "yellow", "white", "black");
+        foreach($colors as $color){
+            echo "<br/>",$color;
+        }
+    ?>
+</body>
+</html>
